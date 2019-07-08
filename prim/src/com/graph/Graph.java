41f7a57eb2324e4055777eea8545e885b222f5eb
Ronnie Yuan <591806780@qq.com>
2019-07-08 13:50:06 +0800

    another model

--- prim/src/com/graph/Graph.java
+++ prim/src/com/graph/Graph.java
@@ -6,42 +6,44 @@ import java.util.Objects;
 
 public class Graph {
     private String[] node;
-    private Integer[][] weight;
+    private Object[][] weight;
     private HashMap<String, String> link = new HashMap<>();
-    private HashMap<String, Integer> linkAndWeight = new HashMap<>();
+    private HashMap<String, Object> linkAndWeight = new HashMap<>();
 
-    public Graph(String[] node, Integer[][] weight) {
+
+    public Graph(String[] node, Object[][] weight) {
         this.node = node;
         this.weight = weight;
     }
 
-    public Integer getNodeSize(){
+    public Integer getNodeSize() {
         return this.getNode().length;
     }
 
-    public Integer getWeightSize(){
+    public Integer getWeightSize() {
         return this.getWeight().length;
     }
 
     /**
-     *  输入节点集合与权重集合, 将节点配对, 再将节点拼成的String与权重配对
+     * 输入节点集合与权重集合, 将节点配对, 再将节点拼成的String与权重配对
+     *
      * @param node
      * @param weight
      * @return
      */
-    public HashMap<String, Integer> getLinkAndWeight(String[] node, Integer[][] weight){
+    public HashMap<String, Integer> getLinkAndWeight(String[] node, Integer[][] weight) {
 
         HashMap<String, Integer> linkAndWeightMap = new HashMap<>();
 
         // String[] combinedLinks = new String[node.length*node.length];
         String combinedLinks = "";
 
-        for (int i = 0; i < this.getNodeSize(); i++){
-            for (int j = 0; j < this.getNodeSize(); j++){
-                if (node[i].equals(node[j])){
+        for (int i = 0; i < this.getNodeSize(); i++) {
+            for (int j = 0; j < this.getNodeSize(); j++) {
+                if (node[i].equals(node[j])) {
                     this.weight[i][j] = 0;
                 }
-                combinedLinks = node[i]+"-"+node[j];
+                combinedLinks = node[i] + "-" + node[j];
                 if (node.length == weight.length) {
 
                     //   System.out.println(combinedLinks);
@@ -58,6 +60,44 @@ public class Graph {
         return linkAndWeightMap;
     }
 
+    /**
+     * 输入节点集合与权重集合, 将节点配对, 再将节点拼成的String与权重配对
+     *
+     * @param node
+     * @param weight
+     * @return
+     */
+    public HashMap<String, Double> getLinkAndWeight2(String[] node, Double[][] weight) {
+
+        HashMap<String, Double> linkAndWeightMap = new HashMap<>();
+
+        // String[] combinedLinks = new String[node.length*node.length];
+        String combinedLinks = "";
+
+        for (int i = 0; i < this.getNodeSize(); i++) {
+            for (int j = 0; j < this.getNodeSize(); j++) {
+                if (node[i].equals(node[j])) {
+                    weight[i][j] = Double.valueOf(0);
+                }
+                combinedLinks = node[i] + " vs " + node[j];
+
+                if (node.length == weight.length) {
+
+                    //   System.out.println(combinedLinks);
+                    //  System.out.println("------------------------");
+                    // System.out.println(weight[i]);
+                    linkAndWeightMap.put(combinedLinks, (Double) weight[i][j]);
+                    //System.out.println(linkAndWeightMap);
+                } else {
+                    System.out.println("节点对数与权重数量不符合");
+                }
+            }
+        }
+
+        return linkAndWeightMap;
+    }
+
+
     public String[] getNode() {
         return node;
     }
@@ -66,7 +106,7 @@ public class Graph {
         this.node = node;
     }
 
-    public Integer[][] getWeight() {
+    public Object[][] getWeight() {
         return weight;
 
     }
@@ -80,15 +120,18 @@ public class Graph {
         if (this == o) return true;
         if (!(o instanceof Graph)) return false;
         Graph graph = (Graph) o;
-        return node.equals(graph.node) &&
-                weight.equals(graph.weight) &&
+        return Arrays.equals(getNode(), graph.getNode()) &&
+                Arrays.equals(getWeight(), graph.getWeight()) &&
                 link.equals(graph.link) &&
                 linkAndWeight.equals(graph.linkAndWeight);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(node, weight, link, linkAndWeight);
+        int result = Objects.hash(link, linkAndWeight);
+        result = 31 * result + Arrays.hashCode(getNode());
+        result = 31 * result + Arrays.hashCode(getWeight());
+        return result;
     }
 
     @Override
@@ -101,3 +144,5 @@ public class Graph {
                 '}';
     }
 }
+
+
--- prim/src/com/graph/GraphTest.java
+++ prim/src/com/graph/GraphTest.java
@@ -11,7 +11,7 @@ public class GraphTest {
         String[] nodes = {"Shanghai", "BeiJing", "Auckland", "Urbana Champaign", "Sydney", "FrankFurt"};
         Integer[][] weights = {{0, 3000, 12000, 17000, 10000, 11000},
                             {3000, 0, 14000, 17000, 12000, 10000},
-                            {3000, 12000, 0, 8000, 3000, 14000},
+                            {12000, 14000, 0, 8000, 3000, 14000},
                             {17000, 17000, 8000, 0, 10000, 15000},
                             {10000, 12000, 3000, 10000, 0, 12000},
                             {11000, 10000, 14000, 15000, 12000, 0}
@@ -19,7 +19,28 @@ public class GraphTest {
 
         Graph graph = new Graph(nodes, weights);
 
-        HashMap<String, Integer> combinedLinks = graph.getLinkAndWeight(graph.getNode(), graph.getWeight());
+        HashMap<String, Integer> combinedLinks = graph.getLinkAndWeight(graph.getNode(), (Integer[][]) graph.getWeight());
         combinedLinks.forEach((k, v)-> System.out.println(k + "=" + v));
+    }
+    @Test
+    public void testGraph2(){
+        /**
+         * 模型 2: Dota2中路英雄对线胜率, 有向但权重不一致图, 设计规律: 除反斜率对角线外, 其他都满足a[i][j] + a[j][i] = 1
+         */
+        String[] mid_lane_heros = {"Shadow Fiend", "Temper Assassin", "Viper", "Invoker", "Brood Mother", "Kunka", "Ember Spirit", "Storm Spirit", "Huskar"};
+        Double[][] weights = {{0.0, 0.2, 0.5, 0.6, 0.4, 0.45, 0.7, 0.7, 0.55},
+                            {0.8, 0.0, 0.3, 0.8, 0.55, 0.45, 0.5, 0.7, 0.3},
+                            {0.5, 0.7, 0.0, 0.6, 0.01, 0.5, 0.6, 0.65, 0.7},
+                            {0.4, 0.2, 0.4, 0.0, 0.001, 0.3, 0.6, 0.6, 0.4},
+                            {0.6, 0.45, 0.99, 0.999, 0.0, 0.3, 0.55, 0.6, 9.9},
+                            {0.55, 0.55, 0.5, 0.7, 0.7, 0.0, 0.7, 0.7, 0.2},
+                            {0.3, 0.5, 0.4, 0.4, 0.45, 0.3, 0.0, 0.45, 0.1},
+                            {0.3, 0.3, 0.35, 0.4, 0.4, 0.3, 0.55, 0.0, 0.4},
+                            {0.45, 0.7, 0.3, 0.6, 0.1, 0.8, 0.9, 0.6, 0.0}};
+
+        Graph graph = new Graph(mid_lane_heros, weights);
+
+        HashMap<String, Double> mid_solo_win_rate = graph.getLinkAndWeight2(graph.getNode(), (Double[][]) graph.getWeight());
+        mid_solo_win_rate.forEach((k, v)-> System.out.println(k + "=" + v));
     }
 }
